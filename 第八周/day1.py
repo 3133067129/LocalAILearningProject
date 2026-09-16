2ca3becc039aaa38b53290ba62093d01b071d7af
UnexistCat <3133067129@qq.com>
2026-09-16 21:42:18 +0800

    第八周第一天读取并处理文本内容

--- 第八周/day1.py
+++ 第八周/day1.py
@@ -1,4 +1,14 @@
-with open("第八周\stock_prices.txt", "r", encoding="utf-8") as f:
-    content = f.read()
-    for i in (content or '').split():
-        print(i, type(i))
+def high_price_date(files_local, fields = []):
+    with open(files_local, "r", encoding="utf-8") as f:
+        for line in f.readlines():
+            clear_line = line.strip()
+            fields.append(clear_line.split(","))
+
+        for field in fields:
+            date_str = field[0]
+            price = float(field[1])
+            if price > 183:
+                print(date_str, price)            
+
+file = r"D:\projects\LocalAILearningProject\第八周\stock_prices.txt"
+high_price_date(file)
